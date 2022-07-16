091d9553632d323818487e6aff47faed08c3c27b
zainzh <395282340@qq.com>
2022-07-16 08:32:51 +0000

    expend lift reward

--- isaacgymenvs/tasks/dual_franka.py
+++ isaacgymenvs/tasks/dual_franka.py
@@ -1620,7 +1620,7 @@ def compute_franka_reward(
                                     + rot_reward_scale * (rot_reward_1 * cf) \
                                     + around_handle_reward_scale * (around_handle_reward_1 * cf) \
                                     + finger_dist_reward_scale * (finger_dist_reward_1 * cf) \
-                                    + 4* (lift_reward_1 * cf) \
+                                    + 20* (lift_reward_1 * cf) \
                                     - action_penalty_scale * action_penalty
                                     - cup_fall_penalty)
     rewards = rewards + stage2 * (dist_reward_stage2 * dist_reward_scale * 20 \
